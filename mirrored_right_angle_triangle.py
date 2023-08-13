765903f97e85ebe7904238b1aa8ee50e62834d06
Rajkumar rajbhar <114279134+rajkumar912639@users.noreply.github.com>
2023-08-13 19:56:14 +0530

    Add files via upload

---
+++ mirrored_right_angle_triangle.py
@@ -0,0 +1,10 @@
+def print_triangle_pattern():
+    row = ''
+    for i in range(1, 2*4, 2):
+        row += str(i)
+        print(row)
+        
+# Read input N
+
+# Call the function to print the pattern
+print_triangle_pattern()
